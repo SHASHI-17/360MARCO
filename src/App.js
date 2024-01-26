fc651b1a68356ff0f8fe14973a6355dc90ee27ae
SHASHI-17 <hibye31583@gmail.com>
2024-01-26 21:36:38 +0530

    ek aur change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "./components/NavBar";
@@ -6,22 +6,39 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";import About from './components/About';
-;
+import { Footer } from "./components/Footer";
+import About from './components/About';
 
 function App() {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handleRouteChange);
+
+    return () => {
+      window.removeEventListener('popstate', handleRouteChange);
+    };
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
-      <Banner />
-      <Skills />
-      <Projects />
-      <Contact />
-      <About/>
-      <Footer />
+      {currentPath === '/' && (
+        <>
+          <Banner />
+          <Skills />
+          <Contact />
+          <Footer />
+        </>
+      )}
+      {currentPath === '/about' && <About />}
+      {currentPath === '/projects' && <Projects />}
     </div>
   );
 }
 
 export default App;
-
